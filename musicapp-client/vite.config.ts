21dfe5119a965211c29decfe6b3f268d5bcc80a6
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-24 14:33:02 -0400

    Moving to more deno

--- musicapp-client/vite.config.ts
+++ musicapp-client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import deno from "@deno/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [deno(), react()],
+	css: {
+		modules: {
+			localsConvention: "camelCase",
+		},
+	},
+});
