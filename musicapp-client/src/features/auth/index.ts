5ecca8db55c7dca8ca01f59d70720fc73eae241f
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-25 14:58:52 -0400

    Fixing imports

--- musicapp-client/src/features/auth/index.ts
+++ musicapp-client/src/features/auth/index.ts
@@ -1,6 +1,6 @@
 import AdminRoute from "./components/AdminRoute/AdminRoute.tsx";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.tsx";
 import Login from "./pages/Login/Login.tsx";
-import { isAdminUser, isAuthenticated } from "./utils/authentication/authentication.ts";
+import { isAdminUser, isAuthenticated } from "./utils/authentication.ts";
 
 export { AdminRoute, isAdminUser, isAuthenticated, Login, ProtectedRoute };
